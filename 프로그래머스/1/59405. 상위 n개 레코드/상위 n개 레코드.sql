7d719d9eba6ba345a49c60f76758c46f6c17306e
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2024-12-08 19:29:27 +0900

    [level 1] Title: 상위 n개 레코드, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
+++ 프로그래머스/1/59405. 상위 n개 레코드/상위 n개 레코드.sql
@@ -1,7 +1,26 @@
--- 코드를 입력하세요
+# -- 코드를 입력하세요
+# SELECT NAME
+# FROM ANIMAL_INS
+# WHERE DATETIME = (
+#     SELECT MIN(DATETIME)
+#     FROM ANIMAL_INS
+# )
+
+
 SELECT NAME
 FROM ANIMAL_INS
-WHERE DATETIME = (
-    SELECT MIN(DATETIME)
-    FROM ANIMAL_INS
-)
+ORDER BY DATETIME
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
