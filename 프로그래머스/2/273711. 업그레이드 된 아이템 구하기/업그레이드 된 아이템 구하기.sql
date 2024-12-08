336550012cd0638dc3685ad13b7b9232fc9abe22
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2024-12-08 20:38:50 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -1,6 +1,39 @@
-SELECT it.ITEM_ID, ii.ITEM_NAME, ii.RARITY
-FROM ITEM_INFO ii
-join item_tree it on ii.item_id = it.item_id
-where it.parent_item_id in (
-select item_id from item_info where rarity = 'rare')
-order by item_id desc;
+# SELECT it.ITEM_ID, ii.ITEM_NAME, ii.RARITY
+# FROM ITEM_INFO ii
+# join item_tree it on ii.item_id = it.item_id
+# where it.parent_item_id in (
+# select item_id from item_info where rarity = 'rare')
+# order by item_id desc;
+
+# SELECT IT.ITEM_ID, II.ITEM_NAME, II.RARITY
+# FROM ITEM_INFO II
+# JOIN ITEM_TREE IT ON II.ITEM_ID = IT.PARENT_ID
+# ORDER BY IT.ITEM_ID DESC;
+
+
+# WHERE ITEM_ID IN (
+#     SELECT ITEM_ID
+#     FROM 
+
+# )
+
+SELECT II.ITEM_ID, II.ITEM_NAME, II.RARITY
+FROM ITEM_INFO AS II
+JOIN ITEM_TREE AS IT ON II.ITEM_ID = IT.ITEM_ID
+WHERE IT.PARENT_ITEM_ID IN (
+SELECT ITEM_ID
+FROM ITEM_INFO
+WHERE RARITY = 'RARE')
+ORDER BY II.ITEM_ID DESC;
+
+
+
+
+
+
+
+
+
+
+
+
