b4d9a393430fefd6b26b037261f30eceb5baa2a6
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-01-04 23:19:42 +0900

    [level 2] Title: 더 맵게, Time: 1545.31 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,27 +1,49 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int s : scoville) {
+        
+        for (int s : scoville) {
             pq.offer(s);
         }
         
-        int answer = 0;
-        while(!pq.isEmpty() && pq.peek() < K) {
-            int first = pq.poll();
-            if (!pq.isEmpty()) {
-                int second = pq.poll();
-                int newS = first + 2*(second);
-                
-                pq.offer(newS);
-                answer += 1;
-            }
-            else {
-                return -1;
-            }
+        int cnt = 0;
+        while (pq.size() >= 2 && pq.peek() < K) {
+            int n = pq.poll() + pq.poll()*2;
+            pq.offer(n);
+            cnt++;
         }
-    
         
-        return answer;
+        if (pq.peek() < K) return -1;
+        return cnt;
     }
 }
+
+// import java.util.*;
+// class Solution {
+//     public int solution(int[] scoville, int K) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         for(int s : scoville) {
+//             pq.offer(s);
+//         }
+        
+//         int answer = 0;
+//         while(!pq.isEmpty() && pq.peek() < K) {
+//             int first = pq.poll();
+//             if (!pq.isEmpty()) {
+//                 int second = pq.poll();
+//                 int newS = first + 2*(second);
+                
+//                 pq.offer(newS);
+//                 answer += 1;
+//             }
+//             else {
+//                 return -1;
+//             }
+//         }
+    
+        
+//         return answer;
+//     }
+// }
