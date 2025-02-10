1d9fc00446ed9d4695863e82f7609f1a814c663b
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-02-10 18:44:17 +0900

    [Gold V] Title: 평범한 배낭, Time: 172 ms, Memory: 53540 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,45 +1,45 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 
-    static int n, k;
-    static int dp[][], w[], v[]; // dp배열과 무게, 가치배열
+    static int n,k;
+    static int[][] dp;
+    static int[] w;
+    static int[] v;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        dp = new int[n+1][k+1];
-
         w = new int[n+1];
         v = new int[n+1];
-
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             w[i] = Integer.parseInt(st.nextToken());
             v[i] = Integer.parseInt(st.nextToken());
         }
 
+        dp = new int[n+1][k+1]; // dp[i][j] = 물건 i 포함 무게 k일 때의 최대 가치 저장
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= k; j++) {
-                // 기본적으로 이전 아이템의 가치를 저장한다.
+                // 이전 물건까지의 최대 가치를 가져온다.
                 dp[i][j] = dp[i-1][j];
-                // 만약 담을 수 있는 무게가 자신의 무게 이상이라면,
+
+                // 현재 물건의 무게가 현재 무게 이하여야 함
                 if (w[i] <= j) {
-                    dp[i][j] = Math.max(dp[i][j], v[i]+dp[i-1][j-w[i]]);
+                    // 현재 물건을 포함했을 때와 안했을 때 중 더 큰 값으로 갱신
+                    dp[i][j] = Math.max(dp[i][j], v[i] + dp[i-1][j-w[i]]);
                 }
             }
         }
 
-        bw.write(String.valueOf(dp[n][k]));
-        bw.flush();
-        bw.close();
-
+        System.out.print(dp[n][k]);
     }
 }
