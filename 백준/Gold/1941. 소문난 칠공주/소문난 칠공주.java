3028aa8cb0284a0a2d4a7b77bf89abac7f708010
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-03-04 18:08:26 +0900

    [Gold III] Title: 소문난 칠공주, Time: 388 ms, Memory: 91820 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,4 +1,3 @@
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,87 +6,100 @@ import java.util.Queue;
 
 public class Main {
 
-    static Character[][] seat;
-    static int result = 0;
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
+    static int n = 5;
+    static char[][] arr;
+    static int cnt = 0;
+    static int[] dr = {0,0,1,-1};
+    static int[] dc = {1,-1,0,0};
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        seat = new Character[5][5];
 
-        for (int i = 0; i < 5; i++) {
+        arr = new char[n][n];
+        for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            for (int j = 0; j < 5; j++) {
-                seat[i][j] = s.charAt(j);
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = s.charAt(j);
             }
         }
 
         int[] selected = new int[7];
-        backtracking(0,0, selected, 0);
-        System.out.print(result);
-
+        // 칠공주를 결성할 수 있는 모든 경우의 수를 구한다.
+        backtracking(0,0,0, selected);
+        System.out.println(cnt);
     }
 
-    // 0-24 중 7명 선택
-    public static void backtracking(int start, int r, int[] selected, int yNum) {
-
-        if (r == 7) {
-            check(selected);
+    // 25개의 문자 중, 7개를 선택한다. (조합)
+    public static void backtracking(int start, int c, int yNum, int[] selected) {
+        if (c == 7) {
+            //System.out.println("확인");
+            // 칠공주 결성 여부 확인
+            if (isSeven(selected)) cnt++;
             return;
         }
 
-        for (int i = start; i < 25; i++) {
-            selected[r] = i;
-            // 선택한 학생까지 포함해, Y가 3이상이면 안됨
-            if (seat[i/5][i%5] == 'Y') {
+        // S가 적어도 4 이상, Y는 최대 3
+        for (int i = start; i < n*n; i++) {
+            char ch = arr[i/n][i%n];
+            if (ch == 'Y') {
                 if (yNum < 3) {
-                    backtracking(i+1, r+1, selected, yNum+1);
+                    selected[c] = i;
+                    backtracking(i+1, c+1, yNum+1, selected);
                 }
             }
-            else {
-                backtracking(i+1, r+1, selected, yNum);
+            else if (ch == 'S') {
+                selected[c] = i;
+                backtracking(i+1, c+1, yNum, selected);
             }
         }
-
     }
 
-    // 선택된 7명의 학생이 연결되어 있는지 확인
-    public static void check(int[] selected) {
+    // 해당 조합이 인접해있는지 확인한다.
+    private static boolean isSeven(int[] selected) {
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {selected[0]/5, selected[0]%5});
-        boolean[] visited = new boolean[7];
-        visited[0] = true;
+        // BFS
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(selected[0]/n, selected[0]%n));
+        //boolean[][] visited = new boolean[n][n];
+        boolean[] v = new boolean[7];
+        v[0] = true;
+
+        //visited[queue.peek().r][queue.peek().c] = true;
         int s = 1;
-
         while (!queue.isEmpty()) {
 
-            int r = queue.peek()[0];
-            int c = queue.poll()[1];
+            Node cur = queue.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dx[i];
-                int nc = c + dy[i];
+                int nr = cur.r + dr[i];
+                int nc = cur.c + dc[i];
 
-                if (nr < 0 || 5 <= nr || nc < 0 || 5 <= nc) {
-                    continue;
-                }
+                if (nr < 0 || nr >= n || nc < 0 || nc >= n) continue;
 
-                // 해당 위치가 선택된 학생 위치 중 하나일 때만 큐에 삽입
+                // 해당 위치가 selected 에 속할 때만 탐색 진행(큐에 삽입)
                 for (int j = 0; j < 7; j++) {
-                    if (selected[j]/5 == nr && selected[j]%5 == nc && !visited[j]) {
-                        visited[j] = true;
+                    if (!v[j] && selected[j] / n == nr && selected[j] % n == nc) {
+                        v[j] = true;
+                        queue.offer(new Node(nr,nc));
                         s++;
-                        queue.offer(new int[] {nr,nc});
                     }
                 }
             }
         }
-
         if (s == 7) {
-            result++;
+            //System.out.println("성공");
+            return true;
+        }
+        return false;
+    }
+
+    static class Node {
+        int r;
+        int c;
+
+        public Node(int r, int c) {
+            this.r = r;
+            this.c = c;
         }
     }
 }
