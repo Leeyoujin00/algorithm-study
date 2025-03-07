c23115243a3781f821fb053edd803a52953c57cb
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-03-07 19:06:38 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,15 +1,28 @@
 -- 코드를 작성해주세요
-select id, 
-    case 
-    when percent_rank() over(order by size_of_colony desc) * 100 <= 25
-    then 'CRITICAL'
-    when percent_rank() over(order by size_of_colony desc) * 100 <= 50
-    then 'HIGH'
-    when percent_rank() over(order by size_of_colony desc) * 100 <= 75
-    then 'MEDIUM'
-    else 'LOW'
-    end
-    as COLONY_NAME
-from ecoli_data
-order by id;
-    
+# select id, 
+#     case 
+#     when percent_rank() over(order by size_of_colony desc) * 100 <= 25
+#     then 'CRITICAL'
+#     when percent_rank() over(order by size_of_colony desc) * 100 <= 50
+#     then 'HIGH'
+#     when percent_rank() over(order by size_of_colony desc) * 100 <= 75
+#     then 'MEDIUM'
+#     else 'LOW'
+#     end
+#     as COLONY_NAME
+# from ecoli_data
+# order by id;
+
+SELECT ID,
+    CASE 
+    WHEN PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) *100 <= 25
+    THEN 'CRITICAL'
+    WHEN PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC)
+    * 100 <= 50
+    THEN 'HIGH'
+    WHEN PERCENT_RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) * 100 <= 75
+    THEN 'MEDIUM'
+    ELSE 'LOW'
+    END AS COLONY_NAME
+FROM ECOLI_DATA
+ORDER BY ID;
