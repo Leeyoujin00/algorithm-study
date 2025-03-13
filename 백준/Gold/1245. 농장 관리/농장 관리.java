2f988ce24519cd691c66a714ac790fd5f4d730f5
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-03-13 17:18:22 +0900

    [Gold V] Title: 농장 관리, Time: 228 ms, Memory: 84308 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-import java.util.*;
 
 public class Main {
     static int n,m;
-    static int[][] farm;
-    static int[] dx = {1,-1,0,0,1,-1,1,-1};
-    static int[] dy = {0,0,1,-1,-1,-1,1,1};
-    static boolean[][] visited;
+    static int[][] map;
+    static int[] dx = {0,0,1,-1,1,-1,-1,1};
+    static int[] dy = {1,-1,0,0,-1,1,-1,1};
+    static boolean[][] v;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,87 +18,93 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        v = new boolean[n][m];
 
-        farm = new int[n][m];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                farm[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        visited = new boolean[n][m];
-        int answer = 0;
+        int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (farm[i][j] != 0 && !visited[i][j]) {
-                    if (bfs(i,j,farm[i][j])) {
-                        //System.out.println(i + " " + j + " " + farm[i][j]);
-                        answer++;
-                    }
+                if (map[i][j] != 0 && !v[i][j] && countMountains(i,j)) {
+                    count++;
+                    bfs(i,j);
                 }
             }
         }
 
-        System.out.println(answer);
-
+        System.out.print(count);
     }
 
-    // 인접한 위치의 높이 같은 격자들 모두 확인
-    static boolean bfs(int startX, int startY, int h) {
+    // 산봉우리의 개수를 센다.
+    // 산봉우리는 팔방의 격자들보다 높아야 한다.
+    // 팔방에 더 높은 격자 없으면 산봉우리임.
+    // bfs 활용
+    private static boolean countMountains(int x, int y) {
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[] {startX,startY});
-        boolean[][] v = new boolean[n][m];
-        v[startX][startY] = true;
-
-        List<int[]> adj = new ArrayList<>();
-        adj.add(new int[] {startX, startY});
-
-        while(!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int x = cur[0];
-            int y = cur[1];
-            // 인접한 격자들 큐에 넣음
-            for (int i = 0; i < 8; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m || v[nx][ny]) continue;
-
-                v[nx][ny] = true;
-                if (farm[nx][ny] == h) {
-                    queue.offer(new int[] {nx,ny});
-                    adj.add(new int[] {nx,ny});
-                }
-            }
-        }
-
-        // 인접한 격자들의 집합의 인접한 곳에 더 높은 위치 없는지 확인
-        for (int[] pos : adj) {
-            if (!check(pos[0], pos[1], h)) return false;
-        }
-
-        return true;
-    }
-
-    // 해당 위치가 봉우리인자 확인
-    // 팔방에 더 높은 곳 있으면 봉우리 아님
-
-    static boolean check(int x, int y, int h) {
-
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[] {x,y});
         visited[x][y] = true;
 
-        // 팔방 확인
-        for (int i = 0; i < 8; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= m || farm[nx][ny] == 0) {
-                continue;
+        int h = map[x][y];
+        while(!que.isEmpty()) {
+            int[] cur = que.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) {
+                    continue;
+                }
+
+                // 인접한 격자 중 자신보다 높은 것 있으면 산봉우리 아니므로 false 반환
+                if (map[nx][ny] > h) {
+                    return false;
+                }
+
+                // 인접한 격자 중 자신과 높이 같은 것 있으면 산봉우리에 포함
+                if (map[nx][ny] == h) {
+                    que.offer(new int[] {nx,ny});
+                    visited[nx][ny] = true;
+                }
             }
-            if (farm[nx][ny] > h) return false;
-            //visited[nx][ny] = true;
+
         }
 
         return true;
+    }
+
+    private static void bfs(int x, int y) {
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{x, y});
+        visited[x][y] = true;
+        v[x][y] = true;
+
+        int h = map[x][y];
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cur[0] + dx[i];
+                int ny = cur[1] + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny]) {
+                    continue;
+                }
+
+                // 인접한 격자 중 자신과 높이 같은 것 있으면 산봉우리에 포함
+                if (map[nx][ny] == h) {
+                    que.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                    v[nx][ny] = true;
+                }
+            }
+        }
     }
 }
