d198e1dce493c9e429a82e6f3446c7d93097e92b
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-03-19 18:45:04 +0900

    [Gold V] Title: 몬스터를 처치하자!, Time: 120 ms, Memory: 14512 KB -BaekjoonHub

--- 백준/Gold/20008. 몬스터를 처치하자！/몬스터를 처치하자！.java
+++ 백준/Gold/20008. 몬스터를 처치하자！/몬스터를 처치하자！.java
@@ -1,63 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
-    static int n, hp;
-    static int[][] skill;
+public class Main{
+    static int n,hp;
+    static int[] c; // 각 스킬의 대기시간 저장
+    static int[] t;
+    static int[] d; // 각 스킬의 대미지 저장
     static int min = Integer.MAX_VALUE;
-    static int[] t; // 각 스킬들의 사용가능한 최소 시간 저장
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         hp = Integer.parseInt(st.nextToken());
-        t = new int[n];
 
-        skill = new int[n][2];
+        c = new int[n];
+        t = new int[n];
+        d = new int[n];
+
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int c = Integer.parseInt(st.nextToken());
-            int d = Integer.parseInt(st.nextToken());
-            skill[i][0] = c;
-            skill[i][1] = d;
-            //available[i] = c;
+            c[i] = Integer.parseInt(st.nextToken());
+            //t[i] = c[i];
+            d[i] = Integer.parseInt(st.nextToken());
         }
 
-        //backtracking(0, hp);
-        dfs(0, hp);
-        System.out.print(min);
+        backtracking(hp, 0);
+        System.out.println(min);
 
     }
 
-    static void dfs(int time, int hp) {
-
-        // 몬스터 죽었으면 최소시간 확인 및 갱신
+    // 현재 사용 가능한 스킬들에 대해 매 초 백트래킹 수행
+    private static void backtracking(int hp, int time) {
+        // 몬스터 처치 완료
         if (hp <= 0) {
             min = Math.min(min, time);
             return;
         }
 
-        // 현재 사용가능한 스킬 있는지 확인
+        // 현재 사용 가능한 스킬 존재여부 저장
         boolean flag = false;
-
         for (int i = 0; i < n; i++) {
-            if (t[i] <= time) {
+            if (t[i] <= time) { // 해당 스킬 사용
                 flag = true;
                 int prev = t[i];
-                t[i] = time+skill[i][0];
-                hp -= skill[i][1];
-                dfs(time+1, hp);
-                hp += skill[i][1];
+                t[i] = time + c[i];
+                hp -= d[i];
+                backtracking(hp, time+1);
+                hp += d[i];
                 t[i] = prev;
             }
         }
 
         if (!flag) {
-            dfs(time+1, hp);
+            backtracking(hp, time+1);
         }
 
     }
