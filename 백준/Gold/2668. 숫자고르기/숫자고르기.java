95c6fc2a97db53a28fef566bd05cc5fc5fc083e2
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-07-08 20:00:51 +0900

    [Gold V] Title: 숫자고르기, Time: 120 ms, Memory: 15712 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,71 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
-
     static int n;
-    static int[] list;
-    static boolean[] visited;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static int[] arr;
+    static boolean[] ck;
+    static Queue<Integer> que = new LinkedList<>();
     static List<Integer> result = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        list = new int[n+1];
 
+        arr = new int[n+1];
         for (int i = 1; i <= n; i++) {
-            list[i] = Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        visited = new boolean[n+1];
+        ck = new boolean[n+1];
+        //StringBuilder result = new StringBuilder();
         for (int i = 1; i <= n; i++) {
-            if (!visited[i]) {
-                pq.clear();
-                // 사이클 생성되지 않는 경로임
-                if (!dfs(i,i)) {
-                    // 방문한 노드들 모두 방문 X 샹태로 복구
-                    while(!pq.isEmpty()) {
-                        visited[pq.poll()] = false;
+            que.clear();
+            if (!ck[i]) {
+                if (!dfs(i, i)) {
+                    while(!que.isEmpty()) {
+                        ck[que.poll()] = false;
                     }
                 }
-                else { // 결과에 저장
-                    while(!pq.isEmpty()) {
-                        result.add(pq.poll());
+                else {
+                    while(!que.isEmpty()) {
+                        result.add(que.poll());
                     }
                 }
             }
         }
 
         Collections.sort(result);
-        StringBuilder sb = new StringBuilder(result.size() + "\n");
-        for (int r : result) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size() + "\n");
+        for (int r: result) {
             sb.append(r + "\n");
         }
 
         System.out.print(sb);
     }
 
-    private static boolean dfs(int curIdx, int startIdx) {
+    private static boolean dfs(int start, int cur) {
 
-        visited[curIdx] = true;
-        pq.offer(curIdx);
-        
-        int nextIdx = list[curIdx];
-        if (nextIdx == startIdx) { // 사이클 생성됐다면, true 반환
+        ck[cur] = true;
+        que.offer(cur);
+
+        int next = arr[cur];
+        if (next == start) {
             return true;
         }
-        // 만약 방문한 적 없는 노드라면, 탐색 진행
-        if (!visited[nextIdx]) {
-            return dfs(nextIdx, startIdx);
+        if (!ck[next]) {
+            return dfs(start, next);
         }
 
-        return false; // 사이클 생성 안되고, 방문한 적 있는 노드라면 탐색 진행 X
+        return false;
     }
 }
