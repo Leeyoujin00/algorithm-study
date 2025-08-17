bcf0382c6fa73324b0dd24a3eb57e6116cb07f55
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-08-17 17:45:39 +0900

    [Gold III] Title: 중량제한, Time: 448 ms, Memory: 55500 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -1,12 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int n,m;
     static List<Node>[] graph;
     static int MAX = 1000000000;
+
+    static class Node {
+        int num;
+        int w;
+        public Node (int num, int w) {
+            this.num = num;
+            this.w = w;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
 
@@ -15,92 +27,55 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[n+1];
 
+        graph = new List[n+1];
         for (int i = 0; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            // a번 섬과 b번 검 사이에 중량제한이 c인 다리가 존재
-            graph[a].add(new Node(b, c));
-            graph[b].add(new Node(a, c));
+
+            graph[a].add(new Node(b,c));
+            graph[b].add(new Node(a,c));
         }
 
-        // 공장이 위치해있는 섬의 번호
         st = new StringTokenizer(br.readLine());
         int f1 = Integer.parseInt(st.nextToken());
         int f2 = Integer.parseInt(st.nextToken());
 
-
-        System.out.print(dijkstra(f1, f2));
-
+        System.out.println(bfs(f1,f2));
     }
 
-    /**
-     * 경로 탐색시, 중량제한 더 큰 값 선택하며 경로 탐색
-     * 만약 목적지에 도달 못하고 끝나는 경로라면 고려X
-     * 계산된 경로 중, 가장 중량제한 작은 값을 답으로 출력
-     */
+    // 시작점 -> 도착점으로 경로 탐색
+    // 경로 선택 기준은, 중량제한 w 값이 가장 큰 경로
+    private static int bfs(int f1, int f2) {
 
-    static class Node {
-        int num;
-        int weight;
+        PriorityQueue<Node> pq = new PriorityQueue<>((o1,o2) -> Integer.compare(o2.w, o1.w));
+        pq.offer(new Node(f1, 0));
+        boolean[] ck = new boolean[n+1];
+        ck[f1] = true;
 
-        public Node(int num, int weight) {
-            this.num = num;
-            this.weight = weight;
-        }
-    }
+        int min = MAX;
+        while(!pq.isEmpty()) {
 
-    static class Node2 {
-        int num;
-        int weight;
-        long wSum;
+            Node cur = pq.poll();
+            ck[cur.num] = true;
+            if (cur.num != f1) min = Math.min(min, cur.w);
 
-        public Node2(int num, int weight, long wSum) {
-            this.num = num;
-            this.weight = weight;
-            this.wSum = wSum;
-        }
-    }
+            if (cur.num == f2) break;
 
-    // f1 -> f2 로 가는 경로 중, 중량제한 값들 중 최솟값 반환
-    private static long dijkstra(int f1, int f2) {
-
-        long min = Long.MAX_VALUE;
-
-        // weight가 큰 경로 먼저 선택해야 함
-        PriorityQueue<Node2> pq = new PriorityQueue<>((o1, o2) -> Long.compare(o2.weight, o1.weight));
-        pq.offer(new Node2(f1, 0, 0));
-        long[] distance = new long[n+1];
-        Arrays.fill(distance, Long.MAX_VALUE);
-        distance[f1] = 0;
-
-        while (!pq.isEmpty()) {
-            Node2 node = pq.poll();
-            //System.out.println("선택 노드 = " + node.num + "중량 = " + node.weight);
-
-            distance[node.num] = node.wSum;
-            if (f1 != node.num) {
-                min = Math.min(min, node.weight); // 최단경로로 가는 weight중 최솟값
-            }
-
-            if (node.num == f2) {
-                break;
-            }
-
-            for (Node next : graph[node.num]) {
-                if (distance[next.num] > distance[node.num] + next.weight) {
-                    pq.offer(new Node2(next.num, next.weight, distance[node.num] + next.weight));
+            // 연결된 경로들 넣음
+            for (Node next : graph[cur.num]) {
+                if (!ck[next.num]) {
+                    pq.offer(next);
                 }
             }
         }
 
         return min;
-
     }
 }
