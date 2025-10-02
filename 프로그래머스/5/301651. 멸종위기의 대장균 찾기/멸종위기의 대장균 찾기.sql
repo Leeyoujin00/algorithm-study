3ea2189cc05cf6abfd01c148a0f87e07c4522be9
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-10-02 15:21:07 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,41 +1,25 @@
--- 코드를 작성해주세요
-# 각 대장균의 세대 구하기
-# with recursive generations as (
-#     select id, parent_id, 1 as gen 
-#     from ecoli_data
-#     where parent_id is null
-#     union all
-#     select e.id, e.parent_id, g.gen+1
-#     from ecoli_data e
-#     join generations g on e.parent_id = g.id
-# )
-# select id from ecoli_data
-# where id not in (select e.parent_id from ecoli_data e
-#                          group by e.parent_id);
-
-with RECURSIVE GENERATIONHIERARCHY as (
-    -- 최초의 대장균(1세대)
-    select ID, PARENT_ID, 1 as GENERATION
+# 자신의 세대 CTE
+# 자식의 수
+WITH RECURSIVE GENERATIONS AS (
+    SELECT ID, PARENT_ID, 1 AS GENERATION
     FROM ECOLI_DATA
     WHERE PARENT_ID IS NULL
-    
     UNION ALL
-    
-    -- 다음 세대 대장균
-    SELECT E.ID, E.PARENT_ID, GH.GENERATION + 1
+    SELECT E.ID, E.PARENT_ID, G.GENERATION + 1
     FROM ECOLI_DATA E
-    JOIN GENERATIONHIERARCHY GH ON E.PARENT_ID = GH.ID
+    JOIN GENERATIONS G ON E.PARENT_ID = G.ID
+),
 
-)
+CHILD_COUNT AS (
+    SELECT PARENT_ID, COUNT(*) AS COUNT
+    FROM ECOLI_DATA
+    GROUP BY PARENT_ID
+) #각 부모의 자식수
 
-select count(*) AS COUNT, GENERATION
-from generationHierarchy
-where id not in (select DISTINCT parent_id from ecoli_data WHERE PARENT_ID IS NOT NULL)
-group by generation
-order by generation
-
-# SELECT ID
-# FROM ECOLI_DATA
-# WHERE ID NOT IN (select DISTINCT parent_id from ecoli_data WHERE PARENT_ID IS NOT NULL)
-
-
+#각 세대별 자식이 없는 대장균의 ID
+SELECT COUNT(*) AS COUNT, GENERATION
+FROM GENERATIONS G
+LEFT JOIN CHILD_COUNT C ON G.ID = C.PARENT_ID
+WHERE C.COUNT IS NULL
+GROUP BY G.GENERATION
+ORDER BY G.GENERATION;
