6424fbd27e5a0d6edf6c3a2d3105b44b07d3ab36
YJLEE <80439068+Leeyoujin00@users.noreply.github.com>
2025-10-02 13:43:38 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -1,21 +1,10 @@
--- 코드를 작성해주세요
-# select id, (select genotype from ecoli_data where id = parent_id) as PARENT_GENOTYPE
-# from ecoli_data
-# where parent_id is not null and ((genotype & (select genotype from ecoli_data where id = parent_id)) = genotype)
-# order by id;
-
-# select id
-# from ecoli_data e
-# where e.parent_id is not null and 
-#                     (e.genotype & (select pe.genotype from ecoli_data pe 
-#                     where pe.id = e.parent_id) = e.genotype);
-                    
-select e.id, e.genotype, p.p_genotype as PARENT_GENOTYPE
-from ecoli_data e
-join (select id, genotype as p_genotype from ecoli_data) as p on e.parent_id = p.id
-where (e.parent_id is not null) and (e.genotype & p.p_genotype) = p.p_genotype
-order by e.id;
-
-
-
-                    
+#부모의 형질을 모두 보유한 대장균의 ID
+#부모 형질 테이블
+SELECT ID, GENOTYPE, PARENT_GENOTYPE
+FROM ECOLI_DATA AS E
+JOIN (
+    SELECT ID AS PARENT_ID, GENOTYPE AS PARENT_GENOTYPE
+    FROM ECOLI_DATA
+) AS G ON E.PARENT_ID = G.PARENT_ID
+WHERE E.GENOTYPE & G.PARENT_GENOTYPE = G.PARENT_GENOTYPE
+ORDER BY ID;
